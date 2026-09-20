b2d207e40dd44cd1d648f1689b18fe5ea1547005
Sufiyan Kureshi <sufiyanrafik313@gmail.com>
2026-09-20 18:53:46 +0530

    Refactor qr code.py by removing comments
    Updated the QR code generation script to remove comments and clean up the code.

--- qr code.py
+++ qr code.py
@@ -1,6 +1,5 @@
 import qrcode
 
-# Jis link ya text ka QR banana hai
 data = "https://github.com/sufiyan1997-bot/python"
 
 print("Generating QR Code...")
@@ -8,7 +7,6 @@ qr = qrcode.QRCode(version=1, box_size=10, border=5)
 qr.add_data(data)
 qr.make(fit=True)
 
-# Image banana aur save karna
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("my_qrcode.png")
 
